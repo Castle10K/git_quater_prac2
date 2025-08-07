883d5c7acd62ee88c34f2930943653ed483b7882
신복집2\robin <robin7575@naver.com>
2025-08-07 18:30:55 +0900

    캐릭터 애니메이션 세팅

--- quater_prac2/Script/player.gd
+++ quater_prac2/Script/player.gd
@@ -7,13 +7,21 @@ const SPEED : float = 8.0
 var accel : float = 10.0
 
 #쳐다보기 변수
-var view_dir : Vector3 = -global_transform.basis.z
+var view_dir : Vector3
 var turn_accel: float = 10.0
 
 #외부변수
 @onready var cam_node : Node3D = $"../Camera" # 카메라 씬
 @onready var cam3d : Camera3D = $"../Camera/CamPar/SpringArm3D/Camera3D" # 카메라 노드
 @onready var target_effect : MeshInstance3D = $"../TargetEffect"
+
+#애니메이션 변수
+@onready var anim_player : AnimationPlayer = $AnimationPlayer
+@onready var anim_tree : AnimationTree = $AnimationTree
+var is_running : bool = false
+var attack_anim_value : float = 0.0
+var blend_pos : Vector2 = Vector2(0.0, 0.0)
+var fin_blend_pos : Vector2
 
 #쉐이더 변수
 var effect_material : ShaderMaterial
@@ -23,11 +31,15 @@ var effect_shader_param : float = 0.0
 var attack_mode : bool = false
 
 func _ready() -> void:
+	view_dir = -global_transform.basis.z
 	effect_material = target_effect.get_active_material(0)
+	anim_tree.active = true
 
 func _process(delta: float) -> void:
 	if attack_mode:
 		effect_shader_param = lerp(effect_shader_param, 1.0, delta * 10.0)
+	else:
+		effect_shader_param = lerp(effect_shader_param, 0.0, delta * 10.0)
 
 func _physics_process(delta: float) -> void:
 	basic_move(delta) #이동 관련
@@ -35,12 +47,19 @@ func _physics_process(delta: float) -> void:
 	mouse_effect(ray_cast(cam3d)) #마우스 레이캐스팅, 반환 포지션에 타겟 이펙트 붙이기
 	if attack_mode:
 		attacking() #공격모드 설정
+	if !attack_mode:
+		running() #러닝모드 설정
+	anim_set()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
 			if !attack_mode:
 				attack_mode = true
+				await get_tree().create_timer(3.0).timeout # 코루틴 ? 병렬처리인가 ? 뭔지 알아볼 필요 있음
+				attack_mode = false
+				
+			
 
 ###############################
 ########## 기능 함수들 ##########
@@ -52,11 +71,19 @@ func basic_move(delta: float) -> void: #이동 관련
 	
 	velocity = lerp(velocity, direction * SPEED, accel * delta)
 	
-	if direction:
-		view_dir = view_dir.slerp(direction, turn_accel * delta) # 서서히 회전
-		look_at(global_position + view_dir)
+	var final_view_dir : Vector3
+	
+	if attack_mode:
+		final_view_dir = target_effect.global_position
+		look_at(final_view_dir, Vector3.UP)
+	else:
+		if direction.length() > 0: # direction 0일땐 아무것도 안하게
+			view_dir = view_dir.slerp(direction, turn_accel * delta) # 서서히 회전
+			final_view_dir = global_position + view_dir
+			look_at(final_view_dir, Vector3.UP)
 	
 	move_and_slide()
+
 
 func cam_follow(delta: float) -> void: #카메라 따라오기
 	cam_node.global_position = global_position
@@ -85,6 +112,32 @@ func mouse_effect(mouse_pos: Vector3) -> void:
 	else:
 		target_effect.visible = true
 		target_effect.global_position = mouse_pos
+	effect_material.set_shader_parameter("factor", effect_shader_param) # 쉐이더 파라미터 설정
 
-func attacking() -> void:
-	effect_material.set_shader_parameter("factor", effect_shader_param)
+func attacking() -> void: #공격모드
+	is_running = false
+	attack_anim_value = lerp(attack_anim_value, 1.0, 0.5) # 블렌드 트리에서 공격모션으로 변경하는 프로퍼티
+	blend_pos = lerp(blend_pos, input_dir, 0.3) # 8way 애니메이션 변환 속도
+	
+	#캐릭터 회전에 맞게 8way 포지션 회전
+	var char_rotate : float = global_basis.get_euler().y
+	var blend_pos_vector3 : Vector3 = Vector3(blend_pos.x, 0, blend_pos.y)
+	var new_rotated_vector3 : Vector3 = blend_pos_vector3.rotated(Vector3.UP, char_rotate)
+	fin_blend_pos = Vector2(new_rotated_vector3.x, new_rotated_vector3.z)
+	fin_blend_pos.normalized()
+
+func running() -> void: #러닝모드
+	attack_anim_value = lerp(attack_anim_value, 0.0, 0.5)
+	if direction:
+		is_running = true
+	else:
+		is_running = false
+
+func anim_set() -> void:
+	#블렌드트리, 러닝 컨디션 파라미
+	anim_tree.set("parameters/attack_select/blend_amount", attack_anim_value)
+	anim_tree.set("parameters/running_state/conditions/start_run", is_running)
+	anim_tree.set("parameters/running_state/conditions/stop_run", !is_running)
+	
+	#사격 이동
+	anim_tree.set("parameters/8way/blend_position", fin_blend_pos)
